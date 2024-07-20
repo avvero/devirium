2e0265bfab4d76573edfaf700db3962638e873c6
belyaev-ay <anton.belyaev@sibit.tech>
2024-07-20 10:28:45 +0700

    update

--- script/random-note.go
+++ script/random-note.go
@@ -11,20 +11,36 @@ import (
 	"time"
 )
 
-// getMdFiles retrieves all .md files in the repository that do not contain the specified tag
-func getMdFiles(root string, tag string) ([]string, error) {
+// getMdFiles retrieves all .md files in the repository that contain the specified tag (or any tag if none specified)
+// and do not contain the draft tag
+func getMdFiles(root string, tag string, excludeTag string) ([]string, error) {
 	var files []string
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() && filepath.Ext(path) == ".md" {
-			containsTag, err := containsTag(path, tag)
+			containsExcludeTag, err := fileContainsTag(path, excludeTag)
 			if err != nil {
 				log.Printf("Error reading file %s: %v", path, err)
 				return nil
 			}
-			if !containsTag {
+			if containsExcludeTag {
+				return nil
+			}
+
+			if tag == "" {
+				// If no tag is specified, include any file without the exclude tag
+				files = append(files, path)
+				return nil
+			}
+
+			containsTag, err := fileContainsTag(path, tag)
+			if err != nil {
+				log.Printf("Error reading file %s: %v", path, err)
+				return nil
+			}
+			if containsTag {
 				files = append(files, path)
 			}
 		}
@@ -33,8 +49,8 @@ func getMdFiles(root string, tag string) ([]string, error) {
 	return files, err
 }
 
-// containsTag checks if a file contains the specified tag
-func containsTag(filePath string, tag string) (bool, error) {
+// fileContainsTag checks if a file contains the specified tag
+func fileContainsTag(filePath string, tag string) (bool, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return false, err
@@ -51,15 +67,20 @@ func containsTag(filePath string, tag string) (bool, error) {
 }
 
 func main() {
+	var tag string
+	if len(os.Args) > 1 {
+		tag = os.Args[1]
+	}
+	excludeTag := "#draft"
 	rand.Seed(time.Now().UnixNano())
 	root := "."
 
-	mdFiles, err := getMdFiles(root, "#debug")
+	mdFiles, err := getMdFiles(root, tag, excludeTag)
 	if err != nil {
 		log.Fatalf("Failed to get .md files: %v", err)
 	}
 	if len(mdFiles) == 0 {
-		log.Fatalf("No valid .md files found without %s tag", "#debug")
+		log.Fatalf("No valid .md files found with %s tag and without %s tag", tag, excludeTag)
 	}
 
 	randomFile := mdFiles[rand.Intn(len(mdFiles))]
